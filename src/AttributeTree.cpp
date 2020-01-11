42cc55e0e4fc6fa499cbb09851e142ecdc2155e4
PoetaKodu <rfpawelsyska@gmail.com>
2020-01-11 01:57:11 +0100

    Implemented AttributeTree

---
+++ src/AttributeTree.cpp
@@ -0,0 +1,57 @@
+#include "AttributeTree.h"
+
+#include <utility>
+
+////////////////////////////////////////////////////////////////////
+AttributeTreeNode::AttributeTreeNode(std::string name_, double value_ )
+	:
+	name( std::move(name_) ),
+	value( value_ )
+{
+}
+
+////////////////////////////////////////////////////////////////////
+AttributeTreeNode::~AttributeTreeNode()
+{
+	if (left)
+		delete left;
+	if (right)
+		delete right;
+}
+
+////////////////////////////////////////////////////////////////////
+void setAttribute(AttributeTreeNode*& treeRoot_, std::string name_, double value_)
+{
+	AttributeTreeNode** processedNode = &treeRoot_;
+	while(*processedNode != nullptr)
+	{
+		auto comp = name_.compare((*processedNode)->name);
+		if (comp < 0)
+			processedNode = &(*processedNode)->left;
+		else if (comp > 0)
+			processedNode = &(*processedNode)->right;
+		else
+		{
+			(*processedNode)->value = value_;
+			return;
+		}
+	}
+	*processedNode = new AttributeTreeNode{ std::move(name_), value_ };
+}
+
+////////////////////////////////////////////////////////////////////
+double getAttributeValue(AttributeTreeNode const* treeRoot_, std::string const & name_)
+{
+	auto processedNode = treeRoot_;
+	while (treeRoot_)
+	{
+		auto comp = name_.compare(treeRoot_->name);
+		if (comp == 0)
+			return treeRoot_->value;
+		else if (comp < 0)
+			treeRoot_ = treeRoot_->left;
+		else
+			treeRoot_ = treeRoot_->right;
+	}
+	throw std::runtime_error("attribute does not exist");
+}
---
+++ src/AttributeTree.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+#include <stdexcept>
+
+class AttributeTreeNode
+{
+public:
+	AttributeTreeNode() = default;
+
+	AttributeTreeNode(std::string name_, double value_ = 0);
+
+	~AttributeTreeNode();
+
+	std::string name;
+	double value = 0;
+
+	// Children
+	AttributeTreeNode* left = nullptr;
+	AttributeTreeNode* right = nullptr;
+};
+
+void setAttribute(AttributeTreeNode*& treeRoot_, std::string name_, double value_);
+
+double getAttributeValue(AttributeTreeNode const* treeRoot_, std::string const & name_);
