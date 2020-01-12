29df106476c0b968b41e88728057e3acbb1eba9c
PoetaKodu <rfpawelsyska@gmail.com>
2020-01-12 23:57:49 +0100

    Implemented sequential reading. Added `private` folder to .gitignore.

--- src/Decision.cpp
+++ src/Decision.cpp
@@ -1,10 +1,14 @@
 #include "Decision.h"
+#include "DecisionTree.h"
 
 #include <iostream>
+#include <fstream>
+#include <stdexcept>
 
 static void displayHelp();
+static std::string readFileSequentially(std::string const& filePath_);
 
-//////////////////////////////////
+/////////////////////////////////////////
 void run(ExecSetup const & exec_)
 {
 	if (exec_.help)
@@ -17,10 +21,21 @@ void run(ExecSetup const & exec_)
 
 		std::cout << "# Reading tree file: \"" << exec_.treeFile << "\"\n";
 
+		DecisionTreeNode* decisionTree = nullptr;
+
+		{
+			std::string fileContents = readFileSequentially(exec_.treeFile);
+			if (fileContents.empty())
+				throw std::runtime_error("tree file is empty or could not be read");
+
+			decisionTree = readTree(fileContents.data(), fileContents.data() + fileContents.size());
+		}
+
 		std::cout << "# Writing output file: \"" << exec_.outputFile << '\"' << std::endl;
 	}
 }
 
+/////////////////////////////////////////
 void displayHelp()
 {
 	constexpr char const* helpContents =
@@ -36,4 +51,26 @@ Usage:
 )help";
 
 	std::cout << helpContents;
+}
+
+/////////////////////////////////////////
+std::string readFileSequentially(std::string const& filePath_)
+{
+	constexpr std::size_t bufferSize = 16 * 1024;
+	constexpr std::size_t reserveSize = 512 * 1024;
+
+	std::string result;
+	result.reserve(reserveSize);
+
+	std::ifstream file(filePath_);
+	if (file.is_open())
+	{
+		char buffer[bufferSize];
+		while(file.read(buffer, bufferSize))
+			result.append(buffer, buffer + bufferSize);
+		result.append(buffer, buffer + file.gcount());
+	}
+	
+	result.shrink_to_fit();
+	return result;
 }
