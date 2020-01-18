9e59464508e5358da173359c837c8f0693f2f0e0
PoetaKodu <rfpawelsyska@gmail.com>
2020-01-18 16:02:22 +0100

    Implemented forward list.

---
+++ src/ForwardList.h
@@ -0,0 +1,40 @@
+#pragma once
+
+template <typename T>
+class ForwardList
+{
+public:
+	class Node
+	{
+		Node(T v)
+			: value(std::move(v))
+		{
+		}
+		T value;
+		Node* next = nullptr;
+	};
+
+	~ForwardList()
+	{
+		this->clear();
+	}
+
+	void clear()
+	{
+		while(head)
+		{
+			Node* temp = head->next;
+			delete head;
+			head = temp;
+		}
+	}
+
+	void push(T value_)
+	{
+		end->next = new Node{ std::move(value_) };
+		end = end->next;
+	}
+
+	Node* head = nullptr;
+	Node* end = nullptr;
+};
