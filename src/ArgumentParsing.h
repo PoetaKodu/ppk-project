d1f88fd9b79341e2bc3417ef9c7388c139fec9e7
PoetaKodu <rfpawelsyska@gmail.com>
2020-01-20 07:37:01 +0100

    Documented file.

--- src/ArgumentParsing.h
+++ src/ArgumentParsing.h
@@ -1,13 +1,20 @@
+/** @file Parsowanie argumentów */
 #pragma once
 
 #include <string>
 
+/** Informacje o trybie uruchomienia. */
 struct ExecSetup
 {
-	std::string inputFile;
-	std::string treeFile;
-	std::string outputFile;
-	bool help = false;
+	std::string inputFile; 	///< Ścieżka do pliku wejściowego rekordów
+	std::string treeFile; 	///< Ścieżka do pliku wejściowego drzewa
+	std::string outputFile; ///< Ścieżka do pliku wyjściowego
+	bool help = false; 		///< Czy wyświetlić pomoc?
 };
 
+/** Parsuje argumenty i wypełnia informacje o trybie uruchomienia.
+ * @param[out] execSetup_ tryb uruchomienia
+ * @param argc_ ilość argumentów
+ * @param argv_ tablica argumentów
+*/
 void parseArguments(ExecSetup & execSetup_, int argc_, char* argv_[]);
