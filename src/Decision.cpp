35bbb1d3e4ede67cadd099c43b121add443c91c9
PoetaKodu <rfpawelsyska@gmail.com>
2020-01-20 10:58:42 +0100

    Moved function declarations to header file.

--- src/Decision.cpp
+++ src/Decision.cpp
@@ -1,51 +1,10 @@
 #include "Decision.h"
-#include "DecisionTree.h"
-
-#include "AttributeTree.h"
-#include "ForwardList.h"
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <iomanip>
 #include <stdexcept>
-
-// Aliasy pomocnicze
-using Record 		= AttributeTree;
-using Records 		= ForwardList< Record >;
-using Category 		= std::pair<std::string, Records>;
-using Categories 	= ForwardList< Category >;
-using Attributes 	= ForwardList< std::string >;
-	
-/** Parsuje plik wejściowy z rekordami, korzystając z drzewa decyzyjnego.
- * @param fileContents_ zawartość pliku wejściowego
- * @param decisionTree_ drzewo decyzyjne
- * @return wygenerowana zawartość pliku wyjściowego
-*/
-static std::string parseInput(std::string fileContents_, DecisionTree const& decisionTree_);
-
-/** Wybiera etykiete danemu rekordowi, korzystając z drzewa decyzyjnego.
- * @param record_ rekord
- * @param decisionTree_ drzewo decyzyjne
- * @return nazwa etykiety
-*/
-static std::string categorize(Record const& record_, DecisionTree const& decisionTree_);
-
-/** Serializuje listę etykiet wraz z przyporządkowanymi rekordami do std::string-a.
- * @param categories_ lista etykiet
- * @param attributes_ lista atrybutów (wymagana przy czytaniu rekordów)
- * @return zserializowana lista etykiet wraz z rekordami
-*/
-static std::string serializeCategories(Categories const& categories_, Attributes const& attributes_);
-
-/** Wczytuje plik sekwencyjnie.
- * @param filePath_ ścieżka do pliku.
- * @return cała zawartość pliku
-*/
-static std::string readFileSequentially(std::string const& filePath_);
-
-/** Wyświetla pomoc programu */
-static void displayHelp();
 
 /////////////////////////////////////////
 void run(ExecSetup const & exec_)
