82cf60a837125cd27b2c9c420381a434a28ed9d9
PoetaKodu <rfpawelsyska@gmail.com>
2020-01-20 07:47:13 +0100

    Removed trailing dot.

--- src/Decision.h
+++ src/Decision.h
@@ -5,6 +5,6 @@
 #include "ArgumentParsing.h"
 
 /** Wykonuje główną logikę programu, korzystając z danych o trybie wykonania.
- * @param exec_ tryb wykonania.
+ * @param exec_ tryb wykonania
 */
 void run(ExecSetup const & exec_);
