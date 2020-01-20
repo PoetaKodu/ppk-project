e3e1a0605dec3ab43c81083763dfd82fcf9c33a3
PoetaKodu <rfpawelsyska@gmail.com>
2020-01-20 07:47:04 +0100

    Documented file.

--- src/Decision.h
+++ src/Decision.h
@@ -1,5 +1,10 @@
+/** @file Główna logika programu. */
+
 #pragma once
 
 #include "ArgumentParsing.h"
 
+/** Wykonuje główną logikę programu, korzystając z danych o trybie wykonania.
+ * @param exec_ tryb wykonania.
+*/
 void run(ExecSetup const & exec_);
