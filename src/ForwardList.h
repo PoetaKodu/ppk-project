3ef7be5806ca328f35499fb208e9d3e40a969cf5
PoetaKodu <rfpawelsyska@gmail.com>
2020-01-20 07:57:16 +0100

    Documented file.

--- src/ForwardList.h
+++ src/ForwardList.h
@@ -1,25 +1,40 @@
+/** @file Szablon listy jednokierunkowej. */
+
 #pragma once
 
+/** Szablon listy jednokierunkowej.
+ * @tparam T typ danych przechowywanych w liście
+*/
 template <typename T>
 class ForwardList
 {
 public:
+
+	/** Węzeł listy. */
 	class Node
 	{
 	public:
-		Node(T v)
-			: value(std::move(v))
+		/**
+		 * Inicjalizuje instancje klasy @see Node.
+		 * @param v wartość do przechowania
+		*/
+		Node(T v_)
+			: value(std::move(v_))
 		{
 		}
-		T value;
-		Node* next = nullptr;
+
+		T value; 				///< przechowywana wartość
+		Node* next = nullptr; 	///< wskaźnik na następny węzeł
 	};
 
+	/** Niszczy instancje szablonu klasy @see ForwardList.
+	*/
 	~ForwardList()
 	{
 		this->clear();
 	}
 
+	/** Czyści listę, zwalniając pamięć. */
 	void clear()
 	{
 		while(head)
@@ -30,6 +45,11 @@ public:
 		}
 	}
 
+	/** Szuka wartości, korzystając z funkcji accept
+	 * @tparam TAcceptFunc typ pozwalający wykonać funkcję o sygnaturze bool(T const&)
+	 * @param accept_ wartość typu TAcceptFunc zwracająca true dla wartości węzła, który ma być zwrócony 
+	 * @return wskaźnik na znaleziony węzeł lub nullptr jeśli nie znaleziono
+	*/
 	template <typename TAcceptFunc>
 	Node* findIf(TAcceptFunc accept_) const
 	{
@@ -43,6 +63,10 @@ public:
 		return t;
 	}
 
+	/** Dodaje wartość na koniec.
+	 * @param value_ wartość do dodania
+	 * @return referencja na węzeł zawierający wartość (ogon listy)
+	*/
 	Node& push(T value_)
 	{
 		auto ptr = new Node{ std::move(value_) };
@@ -56,6 +80,6 @@ public:
 		return *tail;
 	}
 
-	Node* head = nullptr;
-	Node* tail = nullptr;
+	Node* head = nullptr; ///< głowa listy (pierwszy element)
+	Node* tail = nullptr; ///< ogon listy (ostatni element)
 };
